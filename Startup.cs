eeb950a746829b066d05a1af5ecdf49ef820a320
Calin <calinleordean@gmail.com>
2019-02-27 22:48:28 -0500

    Register DBContext to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using todoM.Models
 
 namespace todoM
 {
@@ -23,6 +26,8 @@ namespace todoM
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
